7c77933fa640727f27d74885df0b8c6779ef1637
JFC2025 <jotaefecano@gmail.com>
2025-08-01 08:50:39 +0100

    final

--- gestor-actividades-escolares/resources/views/welcome.blade.php
+++ gestor-actividades-escolares/resources/views/welcome.blade.php
@@ -4,9 +4,16 @@
     <p>Bienvenido al Gestor de Actividades Escolares.</p>
     <p>Aquí puedes gestionar las actividades, alumnos e inscripciones fácilmente.</p>
 
-    <div class="d-flex justify-content-end mt-3">
-        <a href="{{ route('alumnos.index') }}" class="btn btn-primary">
+    <!-- Imagen centrada
+    <div class="text-center my-4">
+        <img src="{{ asset('css/imagen/escuela.jpg') }}" alt="Logo Escuela" width="375" class="mb-3">
+    </div> -->
+
+    <!-- Botón centrado, rojo y grande -->
+    <div class="text-center mt-3">
+        <a href="{{ route('alumnos.index') }}" class="btn btn-danger btn-lg px-5 py-3" style="font-size: 1.5rem;">
             Inicio
         </a>
     </div>
 @endsection
+s
